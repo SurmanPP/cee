d827b232fb6db69938f88011129a4412ef61d0bd
Surman <gregerrelis@gmail.com>
2021-05-04 14:31:20 +0200

    Fix macro length limit and add example

---
+++ e.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "cee.h"
+ee eeeeeeeeee eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee 
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee 
+eeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeee eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee e eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
