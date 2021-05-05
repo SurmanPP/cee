cb4792e9c4d7c79e0d02be1e7feed18a4b4b9fb8
Surman <gregerrelis@gmail.com>
2021-05-05 14:53:46 +0200

    Rewrite the ceefyer

--- ceefy.v
+++ ceefy.v
@@ -1,45 +1,94 @@
 module main
 
 import os
+import cli
 
-fn print_usage() {
-	println('USAGE: eify [input]')
-}
+const (
+	max_name_len = 37
+)
 
-fn eefy(input []string) string {
-	mut output := []string{}
+fn header_from_keywords(ikeywords []string) string {
+	mut keywords := ikeywords.clone()
+	mut result := []string{}
 	mut e := ''
 	mut big_e := 0
-	for keyword in input {
-		if e.len >= 37 {
+	mut last_escaped := false
+	mut last := ''
+	for mut line in keywords {
+		if last_escaped {
+			line = line.add(last)
+			last_escaped = false
+		}
+		if line.ends_with('\\') {
+			last_escaped = true
+			last = '$line'
+			continue
+		}
+		if e.len >= max_name_len {
 			big_e++
 			e = ''
 		}
-		if big_e <= e.len {
+		if big_e > e.len {
+			e += 'E'
+		} else {
 			e += 'e'
-			output << '#define $e $keyword\n'
-			continue
 		}
-		e += 'E'
-		output << '#define $e $keyword\n'
+		result << '#define $e $line'
 	}
-	return output.join('')
+	return result.join('\n')
+}
+
+fn write(cmd cli.Command, input string) ? {
+	if cmd.flags.get_bool('to-stdout') ? {
+		println(input)
+		return
+	}
+	if cmd.flags.get_string('output') ? != '' {
+		if cmd.args.len < 1 {
+			writ_file('${string(4)}.h')
+			return
+		}
+		write_file(cmd.args[0].trim_suffix('.cefy').add('.h'), input)
+	}
 }
 
 fn main() {
-	args := os.args[1..]
-	if os.is_atty(os.stdin().fd) < 1 {
-		print(eefy(os.get_lines()))
-		return
+	mut command := cli.Command{
+		name: 'ceefy'
+		description: 'ceefy your code'
+		usage: 'ceefy [OPTIONS] <input>'
+		version: '2.0.1'
+		execute: fn (cmd cli.Command) ? {
+			if cmd.args.len > 1 {
+				return error('too many arguments: expected 1 got $cmd.args.len')
+			}
+			if os.is_atty(os.stdin().fd) != 1 {
+				write(cmd, header_from_keywords(os.get_lines())) ?
+				return
+			}
+			if cmd.args.len < 1 {
+				return error('not enough arguments: exspected 1 got $cmd.args.len')
+			}
+			if !os.is_file(cmd.args[0]) {
+				return error('file not found: ${cmd.args[0]}')
+			}
+			write(cmd, header_from_keywords(os.read_lines(cmd.args[0]) ?)) ?
+		}
+		flags: [
+			cli.Flag{
+				flag: .bool
+				name: 'to-stdout'
+				abbrev: 's'
+				description: 'write to a file'
+			},
+			cli.Flag{
+				flag: .string
+				name: 'output'
+				abbrev: 'o'
+				description: 'define the output file'
+			},
+		]
+		required_args: 0
 	}
-	if args.len != 1 {
-		print_usage()
-		return
-	}
-	lines := os.read_lines(args[0]) or {
-		eprintln(err)
-		return
-	}
-	print(eefy(lines))
-	return
+	command.parse(os.args)
 }
